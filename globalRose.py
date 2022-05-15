93fff934774bb2c41d8c4b8e3798d2b6905401a7
Benjamin Domond <benjidomond@gmail.com>
2022-05-14 20:25:24 -0400

    testing storefront

--- globalRose.py
+++ globalRose.py
@@ -1,2 +1,23 @@
 # Importing webdriver API to control the browser
 from selenium import webdriver
+# Service object handles browser driver, used to pass ChromeDriverManager config as chrome webdriver is initialized
+from selenium.webdriver.chrome.service import Service
+# Options instance defines what the browser must support and how it should behave
+from selenium.webdriver.chrome.options import Options
+# ChromeDriverManager automatically fetches the most recent version of chrome webdriver and installs it
+from webdriver_manager.chrome import ChromeDriverManager
+# Importing By functionality to use when finding elements with driver
+from selenium.webdriver.common.by import By
+
+#Configure and initializes the instance of chrome webdriver, options and service behavior defined above
+chrome_options = Options()
+# Session start
+driver = webdriver.Chrome(options = chrome_options, service=Service(ChromeDriverManager().install()))
+
+# Function to access webpage
+def getStoreFront():
+    driver.get("https://www.globalrose.com")
+    driver.quit()
+    
+#Calling getStoreFront to access the main page
+getStoreFront()
